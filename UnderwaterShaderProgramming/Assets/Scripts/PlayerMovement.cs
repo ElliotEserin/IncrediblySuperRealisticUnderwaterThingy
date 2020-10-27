0cf395c494f3afaf633beca876d8c29645234335
ElliotE <ellioteserin@gmail.com>
2020-10-27 11:41:55 +0000

    A commit after working on player movement and adjustments.

--- UnderwaterShaderProgramming/Assets/Scripts/PlayerMovement.cs
+++ UnderwaterShaderProgramming/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform mainCamera;
     public float speed;
+    public float acceleration;
 
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Rise");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + Vector3.up * y + transform.forward * z;
+        //Vector3 move = transform.right * x + Vector3.up * y + transform.forward * z;
+        Vector3 move = transform.right * x + Vector3.up * y + mainCamera.forward * z;
+        Vector3 distance = Vector3.Lerp(controller.velocity, move * speed, acceleration * Time.deltaTime);
+        controller.Move(distance * Time.deltaTime);
 
-        controller.Move(move * speed * Time.deltaTime);
+        //Debug
+        if(Input.GetKeyDown(KeyCode.P)) Debug.Break();
     }
 }
