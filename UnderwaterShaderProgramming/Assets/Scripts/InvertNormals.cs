f490921892abbfc9547b1ad81e621150db822807
ElliotE <ellioteserin@gmail.com>
2020-11-05 13:08:51 +0000

    Commit after adding higher resolution caustics and making minor changes to graphics.

---
+++ UnderwaterShaderProgramming/Assets/Scripts/InvertNormals.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[ExecuteAlways]
+public class InvertNormals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
